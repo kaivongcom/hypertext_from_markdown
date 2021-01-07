d7d8941e1772eba7ce85dae6d3e297f590eb231f
kai chan vong <234134+kaichanvong@users.noreply.github.com>
2021-01-07 09:13:41 +0000

    add and remove tests

--- tests/tests_a_lesson_in_this_to_that.rb
+++ tests/tests_a_lesson_in_this_to_that.rb
@@ -38,31 +38,31 @@ class TestMyMarkdownToHypertextParser < Test::Unit::TestCase
 		assert_equal_of_MyMarkdownToHypertextParser(markdown, expected, false)
 	end
 
-	def test_to_links_short
+	def test_text_left_side_to_short_link_in_paragraph
 		markdown = "This is an [example link](/example/ 'With a Title')."
 		expected = "<p>This is an <a href='/example/' title=\"With a Title\">example link</a>.</p>"
 		assert_equal_of_MyMarkdownToHypertextParser(markdown, expected)
 	end
 
-	def test_to_link_in_paragraph
+	def test_text_left_side_to_http_link_in_paragraph
 		markdown = "This is an [example link](http://example.com/ \"With a Title\")."
 		expected = "<p>This is an <a href='http://example.com/' title=\"With a Title\">example link</a>.</p>"
 		assert_equal_of_MyMarkdownToHypertextParser(markdown, expected)
 	end
 
-	def test_link_inside_paragraph_2
-		md = "here is [a example link](http://example.com/ \"With a Title\") to something else"
-		act = "<p>here is <a href='http://example.com/' title=\"With a Title\">a example link</a> to something else</p>"
-		assert_equal_of_MyMarkdownToHypertextParser(md, act)
+	def test_text_right_side_to_short_link_in_paragraph
+		markdown = "[example link](/example/ 'With a Title') for that."
+		expected = "<p><a href='/example/' title=\"With a Title\">example link</a> for that.</p>"
+		assert_equal_of_MyMarkdownToHypertextParser(markdown, expected)
 	end
 
-	def test_wrapper_paragraph_3
-		actual = "<p>here is <a href='http://example.com/' title=\"With a Title\">a example link</a> to something else</p>"
+	def test_wrapped_in_text_link_paragraph
 		md = "here is [a example link](http://example.com/ \"With a Title\") to something else"
+		actual = "<p>here is <a href='http://example.com/' title=\"With a Title\">a example link</a> to something else</p>"
 		assert_equal_of_MyMarkdownToHypertextParser(md, actual)
 	end
 
-	def test_specific_class_name
+	def test_class_name_paragraph
 		actual = "<p class=\"new-class\">here is <a href='http://example.com/' title=\"With a Title\">a example link</a> to something else</p>"
 		md = "here is [a example link](http://example.com/ \"With a Title\") to something else"
 		assert_equal_of_MyMarkdownToHypertextParser(md, actual, 'p', 'new-class')
@@ -80,12 +80,6 @@ class TestMyMarkdownToHypertextParser < Test::Unit::TestCase
 		assert_equal_of_MyMarkdownToHypertextParser(md, actual, 'strong')
 	end
 
-	# def test_link_inside_p_2
-	# 	act = "<p>here is <a href='http://example.com/' title=\"With a Title\">a example link</a> to something (totally) else</p>"
-	# 	md = "here is [a example link](http://example.com/ \"With a Title\") to something (totally) else"
-	# 	actual_expected(md, act)
-	# end
-
 	def test_complex_link
 		act = "<a href='https://drafts.csswg.org/mediaqueries-5/#descdef-media-prefers-reduced-data' title=\"Media Queries Level 5 Editor’s Draft, 21 November 2020\">CSS prefers-reduced-data media-query</a>"
 		md = "[CSS prefers-reduced-data media-query](https://drafts.csswg.org/mediaqueries-5/#descdef-media-prefers-reduced-data 'Media Queries Level 5 Editor’s Draft, 21 November 2020')"
@@ -93,4 +87,3 @@ class TestMyMarkdownToHypertextParser < Test::Unit::TestCase
 	end
 
 end
-
